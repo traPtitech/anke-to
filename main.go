cf205a2638941dee31dae8becc8e564d954be465
mds_boy <modasokun@gmail.com>
2018-07-30 00:07:42 +0900

    質問を取得、追加できるようにした

--- main.go
+++ main.go
@@ -27,6 +27,18 @@ type questionnaires struct {
     Modified_at     time.Time   `json:"modified_at"     db:"modified_at"`
 }
 
+type questions struct {
+    ID                  int         `json:"id"                  db:"id"`
+    Questionnaire_id    int         `json:"questionnaire_id"    db:"questionnaire_id"`
+    Page_num            int         `json:"page_num"            db:"page_num"`
+    Question_num        int         `json:"question_num"        db:"question_num"`
+    Type                string      `json:"type"                db:"type"`
+    Body                string      `json:"body"                db:"body"`
+    Is_requrired        bool        `json:"is_required"         db:"is_required"`
+    Is_deleted          bool        `json:"is_deleted"          db:"is_deleted"`
+    Created_at          time.Time   `json:"created_at"          db:"created_at"`
+}
+
 func establishConnection() (*sqlx.DB, error) {
     user := os.Getenv("MARIADB_USERNAME")
     if user == "" {
@@ -70,13 +82,37 @@ func getQuestionnaire(c echo.Context) error {
     return c.JSON(http.StatusOK, allquestionnaires)
 }
 
+func getQuestions(c echo.Context) error {
+    questionnaireID := c.Param("id")
+    // 質問一覧の配列
+    allquestions := []questions{}
+
+    // アンケートidの一致する質問を取る
+    err := db.Select(&allquestions, "SELECT * FROM questions WHERE questionnaire_id = ?", questionnaireID)
+
+    // エラー処理
+    if err != nil {
+        return c.JSON(http.StatusInternalServerError, err)
+    }
+
+    // 構造体の定義で書いたJSONのキーで変換される
+    return c.JSON(http.StatusOK, allquestions)
+}
+
 func postQuestionnaire(c echo.Context) error {
 
     // リクエストで投げられるJSONのスキーマ
     // その場で構造体を定義もできる
     req := struct {
-        Title string `json:"title"`
-        Is_accepting bool `join:"is_accepting"`
+        Title           string  `json:"title"`
+        Is_accepting    bool    `json:"is_accepting"`
+        Res_shared_to   string  `json:"res_shared_to"`
+        Questions []struct {
+            Page_num        int     `json:"page_num"`
+            Type            string  `json:"type"`
+            Body            string  `json:"body"`
+            Is_required     bool    `json:"is_required"`
+        }
     }{}
 
     // JSONを構造体につける
@@ -88,21 +124,39 @@ func postQuestionnaire(c echo.Context) error {
     if req.Title == "" {
         return c.JSON(http.StatusBadRequest, echo.Map{"error": "name is null"})
     }
+    
+    if req.Res_shared_to == "" {
+        req.Res_shared_to = "administrators"
+    }
 
     // アンケートの追加
-    result := db.MustExec("INSERT INTO questionnaires (title, is_accepting) VALUES (?, ?)", req.Title, req.Is_accepting)
+    result := db.MustExec(
+        "INSERT INTO questionnaires (title, is_accepting, res_shared_to) VALUES (?, ?, ?)", 
+        req.Title, req.Is_accepting, req.Res_shared_to)
 
-    // いい感じに返す
+    // エラーチェック
     lastID, err := result.LastInsertId()
     if err != nil {
         return c.JSON(http.StatusInternalServerError, err)
     }
+
+    for i, v := range req.Questions {
+        res := db.MustExec(
+            "INSERT INTO questions (questionnaire_id, page_num, question_num, type, body, is_required) VALUES (?, ?, ?, ?, ?, ?)",
+            lastID, v.Page_num, i+1, v.Type, v.Body, v.Is_required)
+
+        _, err2 := res.LastInsertId()
+        if err2 != nil {
+            return c.JSON(http.StatusInternalServerError, err2)
+        }
+    }
+
     t := questionnaires{
         ID:             int(lastID),
         Title:          req.Title,
         Is_accepting:   req.Is_accepting,
         Is_deleted:     false,
-        Res_shared_to:  "administrators",
+        Res_shared_to:  req.Res_shared_to,
         Created_at:     time.Now(),
         Modified_at:    time.Now(),
     }
@@ -167,6 +221,7 @@ func main(){
     e.POST("/questionnaire", postQuestionnaire)
     e.PATCH("/questionnaire/:id", editQuestionnaire)
     e.DELETE("/questionnaire/:id", deleteQuestionnaire)
+    e.GET("/questionnaire/:id", getQuestions)
 
     // Start server
     e.Logger.Fatal(e.Start(":1323"))
