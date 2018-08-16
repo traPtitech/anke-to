3c97e6620453409724762eada3a96a306cc999aa
mds_boy <modasokun@gmail.com>
2018-08-16 15:32:54 +0900

    /questionnairesのGET/POSTは定義通り返すようにした(parameterはまだ

--- router.go
+++ router.go
@@ -14,6 +14,7 @@ import (
 type questionnaires struct {
     ID              int             `json:"questionnaireID" db:"id"`
     Title           string          `json:"title"           db:"title"`
+    Description     sql.NullString  `json:"description"     db:"description"`
     Res_time_limit  mysql.NullTime  `json:"res_time_limit"  db:"res_time_limit"`
     Deleted_at      mysql.NullTime  `json:"deleted_at"      db:"deleted_at"`
     Res_shared_to   string          `json:"res_shared_to"   db:"res_shared_to"`
@@ -31,6 +32,22 @@ type questions struct {
     Is_requrired        bool            `json:"is_required"         db:"is_required"`
     Deleted_at          mysql.NullTime  `json:"deleted_at"          db:"deleted_at"`
     Created_at          time.Time       `json:"created_at"          db:"created_at"`
+}
+
+func timeConvert(time mysql.NullTime) string {
+    if time.Valid {
+        return time.Time.String()
+    } else {
+        return "NULL"
+    }
+}
+
+func stringConvert(s sql.NullString) string {
+    if s.Valid {
+        return s.String
+    } else {
+        return ""
+    }
 }
 
 func getID(c echo.Context) error {
@@ -52,9 +69,75 @@ func getQuestionnaire(c echo.Context) error {
     if err != nil {
         return c.JSON(http.StatusInternalServerError, err)
     }
+/*
+    type questionnairesInfo struct {
+        ID              int             `json:"questionnaireID"`
+        Title           string          `json:"title"`
+        Description     string          `json:"description"`
+        Res_time_limit  string          `json:"res_time_limit"`
+        Deleted_at      string          `json:"deleted_at"`
+        Res_shared_to   string          `json:"res_shared_to"`
+        Created_at      time.Time       `json:"created_at"`
+        Modified_at     time.Time       `json:"modified_at"`
+        Is_targeted     bool            `json:"is_targeted"`
+    }
+    var ret []questionnairesInfo
+
+    for _, v := range allquestionnaires {
+        var po questionnairesInfo
+        po.ID = v.ID
+        po.Title = v.Title
+        if v.Description.Valid {
+            po.Description = v.Description.String
+        } else {
+            po.Description = ""
+        }
+        if v.Res_time_limit.Valid {
+            po.Res_time_limit = v.Res_time_limit.Time.String()
+        } else {
+            po.Res_time_limit = "NULL"
+        }
+        if v.Deleted_at.Valid {
+            po.Deleted_at = v.Deleted_at.Time.String()
+        } else {
+            po.Deleted_at = "NULL"
+        }
+        po.Res_shared_to = v.Res_shared_to
+        po.Created_at = v.Created_at
+        po.Modified_at = v.Modified_at
+        po.Is_targeted = true;
+        ret = append(ret, po)
+    }*/
+    type questionnairesInfo struct {
+        ID              int             `json:"questionnaireID"`
+        Title           string          `json:"title"`
+        Description     string          `json:"description"`
+        Res_time_limit  string          `json:"res_time_limit"`
+        Deleted_at      string          `json:"deleted_at"`
+        Res_shared_to   string          `json:"res_shared_to"`
+        Created_at      time.Time       `json:"created_at"`
+        Modified_at     time.Time       `json:"modified_at"`
+        Is_targeted     bool            `json:"is_targeted"`
+    }
+    var ret []questionnairesInfo
+
+    for _, v := range allquestionnaires {
+        ret = append(ret,
+            questionnairesInfo{
+                v.ID,
+                v.Title,
+                stringConvert(v.Description),
+                timeConvert(v.Res_time_limit),
+                timeConvert(v.Deleted_at),
+                v.Res_shared_to,
+                v.Created_at,
+                v.Modified_at,
+                // とりあえず仮でtrueにしている
+                true })
+    }
 
     // 構造体の定義で書いたJSONのキーで変換される
-    return c.JSON(http.StatusOK, allquestionnaires)
+    return c.JSON(http.StatusOK, ret)
 }
 
 func getQuestions(c echo.Context) error {
@@ -79,14 +162,11 @@ func postQuestionnaire(c echo.Context) error {
     // リクエストで投げられるJSONのスキーマ
     req := struct {
         Title           string      `json:"title"`
+        Description     string      `json:"description"`
         Res_time_limit  time.Time   `json:"res_time_limit"`
         Res_shared_to   string      `json:"res_shared_to"`
-        Questions []struct {
-            Page_num        int     `json:"page_num"`
-            Type            string  `json:"type"`
-            Body            string  `json:"body"`
-            Is_required     bool    `json:"is_required"`
-        }
+        Targets         []string    `json:"targets"`
+        Administrators  []string    `json:"administrators"`
     }{}
 
     // JSONを構造体につける
@@ -104,19 +184,16 @@ func postQuestionnaire(c echo.Context) error {
     }
 
     var result sql.Result
-    var res_time_limit mysql.NullTime
 
     // アンケートの追加    
     if req.Res_time_limit.IsZero() {
         result = db.MustExec(
             "INSERT INTO questionnaires (title, res_shared_to) VALUES (?, ?)", 
             req.Title, req.Res_shared_to)
-        res_time_limit.Valid = false
     } else {
         result = db.MustExec(
             "INSERT INTO questionnaires (title, res_time_limit, res_shared_to) VALUES (?, ?, ?)", 
             req.Title, req.Res_time_limit, req.Res_shared_to)
-        res_time_limit = mysql.NullTime{req.Res_time_limit, true}
     }
 
     // エラーチェック
@@ -125,30 +202,36 @@ func postQuestionnaire(c echo.Context) error {
         return c.JSON(http.StatusInternalServerError, err)
     }
 
-    for i, v := range req.Questions {
-        res := db.MustExec(
-            "INSERT INTO questions (questionnaire_id, page_num, question_num, type, body, is_required) VALUES (?, ?, ?, ?, ?, ?)",
-            lastID, v.Page_num, i+1, v.Type, v.Body, v.Is_required)
-
-        _, err2 := res.LastInsertId()
-        if err2 != nil {
-            return c.JSON(http.StatusInternalServerError, err2)
+    for _, v := range req.Targets {
+        _, err := db.Exec(
+            "INSERT INTO targets (questionnaire_id, user_traqid) VALUES (?, ?)",
+            lastID, v)
+        if err != nil {
+            return c.JSON(http.StatusInternalServerError, err)
         }
     }
 
-    var deleted_at mysql.NullTime
-    deleted_at.Valid = false
-
-    t := questionnaires{
-        ID:             int(lastID),
-        Title:          req.Title,
-        Res_time_limit: res_time_limit,
-        Deleted_at:     deleted_at,
-        Res_shared_to:  req.Res_shared_to,
-        Created_at:     time.Now(),
-        Modified_at:    time.Now(),
+    for _, v := range req.Administrators {
+        _, err := db.Exec(
+            "INSERT INTO administrators (questionnaire_id, user_traqid) VALUES (?, ?)",
+            lastID, v)
+        if err != nil {
+            return c.JSON(http.StatusInternalServerError, err)
+        }
     }
-    return c.JSON(http.StatusCreated, t)
+
+    return c.JSON(http.StatusCreated, map[string]interface{}{
+        "questionnaireID":  int(lastID),
+        "title":            req.Title,
+        "description":      req.Description,
+        "res_time_limit":   req.Res_time_limit,
+        "deleted_at":       "NULL",
+        "created_at":       time.Now(),
+        "modified_at":      time.Now(),
+        "res_shared_to":    req.Res_shared_to,
+        "targets":          req.Targets,
+        "administrators":   req.Administrators,
+    })
 }
 
 func editQuestionnaire(c echo.Context) error {
