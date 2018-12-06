b49c8dc3dcfbed6167971f7a21822185a748b292
mds_boy <modasokun@gmail.com>
2018-12-06 10:01:52 +0900

    バグ修正、ファイル分割

--- api.go
+++ api.go
@@ -10,37 +10,6 @@ import (
 	"github.com/labstack/echo"
 )
 
-type questionnaires struct {
-	ID           int            `json:"questionnaireID" db:"id"`
-	Title        string         `json:"title"           db:"title"`
-	Description  string         `json:"description"     db:"description"`
-	ResTimeLimit mysql.NullTime `json:"res_time_limit"  db:"res_time_limit"`
-	DeletedAt    mysql.NullTime `json:"deleted_at"      db:"deleted_at"`
-	ResSharedTo  string         `json:"res_shared_to"   db:"res_shared_to"`
-	CreatedAt    time.Time      `json:"created_at"      db:"created_at"`
-	ModifiedAt   time.Time      `json:"modified_at"     db:"modified_at"`
-}
-
-type questions struct {
-	ID              int            `json:"id"                  db:"id"`
-	QuestionnaireId int            `json:"questionnaireID"     db:"questionnaire_id"`
-	PageNum         int            `json:"page_num"            db:"page_num"`
-	QuestionNum     int            `json:"question_num"        db:"question_num"`
-	Type            string         `json:"type"                db:"type"`
-	Body            string         `json:"body"                db:"body"`
-	IsRequrired     bool           `json:"is_required"         db:"is_required"`
-	DeletedAt       mysql.NullTime `json:"deleted_at"          db:"deleted_at"`
-	CreatedAt       time.Time      `json:"created_at"          db:"created_at"`
-}
-
-type scaleLabels struct {
-	ID              int    `json:"questionID" db:"question_id"`
-	ScaleLabelRight string `json:"scale_label_right" db:"scale_label_right"`
-	ScaleLabelLeft  string `json:"scale_label_left"  db:"scale_label_left"`
-	ScaleMin        int    `json:"scale_min" db:"scale_min"`
-	ScaleMax        int    `json:"scale_max" db:"scale_max"`
-}
-
 func timeConvert(time mysql.NullTime) string {
 	if time.Valid {
 		return time.Time.String()
@@ -221,7 +190,7 @@ func getQuestions(c echo.Context) error {
 				ScaleLabelRight: scalelabel.ScaleLabelRight,
 				ScaleLabelLeft:  scalelabel.ScaleLabelLeft,
 				ScaleMin:        scalelabel.ScaleMin,
-				ScaleMax:        scalelabel.ScaleMin,
+				ScaleMax:        scalelabel.ScaleMax,
 			})
 	}
 
