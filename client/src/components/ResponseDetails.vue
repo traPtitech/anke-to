be61487521c946b46793408238aed44401ead55b
60° <hatsumi.yamane.4609@gmail.com>
2018-12-28 04:42:26 +0900

    EditResponse.vue の名前を ResponseDetails.vue に変えた

---
+++ client/src/components/ResponseDetails.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="is-fullheight">
+    <div class="tabs is-centered">
+      <ul></ul>
+      <a :href="editButtonLink" id="edit-button" :class="{'is-editing': isEditing}">
+        <span class="ti-pencil"></span>
+      </a>
+    </div>
+    <div :class="{'is-editing' : isEditing}" class="is-fullheight">
+      <p>responseId = {{ responseId }} の回答詳細画面</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+// import <componentname> from '<path to component file>'
+
+export default {
+  name: 'ResponseDetails',
+  components: {
+  },
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  computed: {
+    responseId () {
+      return this.$route.params.id
+    },
+    isEditing () {
+      if (this.$route.hash === '#edit') {
+        return true
+      }
+      return false
+    },
+    editButtonLink () {
+      if (!this.isEditing) {
+        return this.responseId + '#edit'
+      } else {
+        return this.responseId
+      }
+    }
+  },
+  mounted () {
+  }
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+.tabs {
+  margin-bottom: 0;
+  margin-right: 0.5rem;
+  margin-left: 0.5rem;
+}
+.tabs:first-child {
+  margin-top: 1rem;
+}
+.is-editing {
+  background-color: #c2c2c2;
+}
+#edit-button {
+  border: #dbdbdb solid 1px;
+}
+</style>
