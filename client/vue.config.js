83f4a028d642edcae7d5c3c5067d22a7dae71454
sapphi-red <green@sapphi.red>
2019-12-30 02:51:58 +0900

    update vue-cli to 4 and others

--- client/vue.config.js
+++ client/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
       // pass options to sass-loader
       sass: {
         // import `src/style/_main.scss` to all components
-        data: `@import "~@/style/_main.scss";`
+        prependData: `@import "~@/style/_main.scss";`
       }
     }
   },
