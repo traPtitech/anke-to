119cdb8ac463a5ce274bd302d8db8bfc9c80288a
YumizSui <buggymercury@gmail.com>
2020-08-15 00:17:30 +0900

    dbタグの削除，UpdateScaleLabels，golint対応

--- model/scale_labels.go
+++ model/scale_labels.go
@@ -7,14 +7,16 @@ import (
 	"github.com/labstack/echo"
 )
 
+//ScaleLabels scale_labelsテーブルの構造体
 type ScaleLabels struct {
-	ID              int    `json:"questionID"        db:"question_id"       gorm:"column:question_id"`
-	ScaleLabelRight string `json:"scale_label_right" db:"scale_label_right" gorm:"column:scale_label_right"`
-	ScaleLabelLeft  string `json:"scale_label_left"  db:"scale_label_left"  gorm:"column:scale_label_left"`
-	ScaleMin        int    `json:"scale_min"         db:"scale_min"         gorm:"column:scale_min"`
-	ScaleMax        int    `json:"scale_max"         db:"scale_max"         gorm:"column:scale_max"`
+	ID              int    `json:"questionID"        gorm:"column:question_id"`
+	ScaleLabelRight string `json:"scale_label_right" gorm:"column:scale_label_right"`
+	ScaleLabelLeft  string `json:"scale_label_left"  gorm:"column:scale_label_left"`
+	ScaleMin        int    `json:"scale_min"         gorm:"column:scale_min"`
+	ScaleMax        int    `json:"scale_max"         gorm:"column:scale_max"`
 }
 
+// GetScaleLabels 指定されたquestionIDのlabelを取得する
 func GetScaleLabels(c echo.Context, questionID int) (ScaleLabels, error) {
 	label := ScaleLabels{}
 	if err := gormDB.Where("question_id = ?", questionID).First(&label).Error; gorm.IsRecordNotFoundError(err) {
@@ -26,6 +28,7 @@ func GetScaleLabels(c echo.Context, questionID int) (ScaleLabels, error) {
 	return label, nil
 }
 
+// InsertScaleLabels IDを指定してlabelを挿入する
 func InsertScaleLabels(c echo.Context, lastID int, label ScaleLabels) error {
 	label.ID = lastID
 	if err := gormDB.Create(&label).Error; err != nil {
@@ -35,24 +38,16 @@ func InsertScaleLabels(c echo.Context, lastID int, label ScaleLabels) error {
 	return nil
 }
 
+// UpdateScaleLabels questionIDを指定してlabelを更新する
 func UpdateScaleLabels(c echo.Context, questionID int, label ScaleLabels) error {
-	labelBefore := ScaleLabels{}
-
-	var err error
-	if labelBefore, err = GetScaleLabels(c, questionID); gorm.IsRecordNotFoundError(err) {
-		return nil
-	} else if err != nil {
-		c.Logger().Error(err)
-		return echo.NewHTTPError(http.StatusBadRequest)
-	}
-
-	if err := gormDB.Model(&labelBefore).Update(&label).Error; err != nil {
+	if err := gormDB.Model(&ScaleLabels{}).Update(&label).Error; err != nil {
 		c.Logger().Error(err)
 		return echo.NewHTTPError(http.StatusInternalServerError)
 	}
 	return nil
 }
 
+// DeleteScaleLabels questionIDを指定してlabelを削除する
 func DeleteScaleLabels(c echo.Context, questionID int) error {
 	if err := gormDB.Where("question_id = ?", questionID).Delete(&ScaleLabels{}).Error; err != nil {
 		c.Logger().Error(err)
