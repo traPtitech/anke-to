5e0f945ffc0257b7cb681956fcb6073b5d0f428c
YumizSui <buggymercury@gmail.com>
2020-09-10 17:32:10 +0900

    scalelabels,update

--- model/scale_labels.go
+++ model/scale_labels.go
@@ -18,7 +18,10 @@ type ScaleLabels struct {
 // GetScaleLabels 指定されたquestionIDのlabelを取得する
 func GetScaleLabels(questionID int) (ScaleLabels, error) {
 	label := ScaleLabels{}
-	if err := gormDB.Where("question_id = ?", questionID).First(&label).Error; gorm.IsRecordNotFoundError(err) {
+	if err := gormDB.
+		Where("question_id = ?", questionID).
+		First(&label).
+		Error; gorm.IsRecordNotFoundError(err) {
 		return ScaleLabels{}, nil
 	} else if err != nil {
 		return ScaleLabels{}, fmt.Errorf("failed to get the scale label (questionID: %d): %w", questionID, err)
@@ -37,7 +40,16 @@ func InsertScaleLabels(lastID int, label ScaleLabels) error {
 
 // UpdateScaleLabels questionIDを指定してlabelを更新する
 func UpdateScaleLabels(questionID int, label ScaleLabels) error {
-	if err := gormDB.Model(&ScaleLabels{}).Update(&label).Error; err != nil {
+	if err := gormDB.
+		Model(&ScaleLabels{}).
+		Where("question_id = ?", questionID).
+		Update(map[string]interface{}{
+			"question_id":       questionID,
+			"scale_label_right": label.ScaleLabelRight,
+			"scale_label_left":  label.ScaleLabelLeft,
+			"scale_min":         label.ScaleMin,
+			"scale_max":         label.ScaleMax}).
+		Error; err != nil {
 		return fmt.Errorf("failed to update the scale labell (questionID: %d): %w", questionID, err)
 	}
 	return nil
@@ -45,7 +57,10 @@ func UpdateScaleLabels(questionID int, label ScaleLabels) error {
 
 // DeleteScaleLabels questionIDを指定してlabelを削除する
 func DeleteScaleLabels(questionID int) error {
-	if err := gormDB.Where("question_id = ?", questionID).Delete(&ScaleLabels{}).Error; err != nil {
+	if err := gormDB.
+		Where("question_id = ?", questionID).
+		Delete(&ScaleLabels{}).
+		Error; err != nil {
 		return fmt.Errorf("failed to delete the scale labell (questionID: %d): %w", questionID, err)
 	}
 	return nil
