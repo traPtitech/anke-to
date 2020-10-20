a16773ce2bc1df33b0d0d194b039c2237c6bea03
mazrean <shunsuke.w04@gmail.com>
2020-10-20 20:42:54 +0900

    COLLATE=utf8mb4_unicode_ci

--- model/db.go
+++ model/db.go
@@ -47,7 +47,7 @@ func EstablishConnection() (*gorm.DB, error) {
 	_db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", user, pass, host, dbname)+"?parseTime=true&loc=Asia%2FTokyo&charset=utf8mb4")
 	db = _db
 	db = db.BlockGlobalUpdate(true)
-	db = db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4")
+	db = db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci")
 
 	return db, err
 }
