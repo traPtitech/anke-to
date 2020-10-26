aa9be899fb11cbc2aed60fe7ac7de51ec763dd7e
mazrean <shunsuke.w04@gmail.com>
2020-10-26 20:12:01 +0900

    recorder作成の関数

--- test/main_test.go
+++ test/main_test.go
@@ -1,12 +1,33 @@
 package test
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/labstack/echo"
 	"github.com/traPtitech/anke-to/model"
 	"github.com/traPtitech/anke-to/router"
+)
+
+type users string
+type httpMethods string
+type contentTypes string
+
+const (
+	userHeader = "X-Showcase-User"
+	userUnAuthorized = "-"
+	userOne users = "mazrean"
+	userTwo users = "ryoha"
+	userThree users = "YumizSui"
+	methodGet httpMethods = http.MethodGet
+	methodPost httpMethods = http.MethodPost
+	methodPatch httpMethods = http.MethodPatch
+	methodDelet httpMethods = http.MethodDelete
+	typeNone contentTypes = ""
+	typeJSON contentTypes = echo.MIMEApplicationJSON
 )
 
 var e *echo.Echo
@@ -30,4 +51,16 @@ func TestMain(m *testing.M) {
 	code := m.Run()
 
 	os.Exit(code)
+}
+
+func createRecorder(user users, method httpMethods, path string, contentType contentTypes, body string) *httptest.ResponseRecorder {
+	req := httptest.NewRequest(string(method), path, strings.NewReader(body))
+	req.Header.Set(echo.HeaderContentType, string(contentType))
+	req.Header.Set(userHeader, string(user))
+
+	rec := httptest.NewRecorder()
+
+	e.ServeHTTP(rec, req)
+
+	return rec
 }
