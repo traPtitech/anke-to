09e30c7d2b4fc9218df1d3912850b58578100bc4
mazrean <shunsuke.w04@gmail.com>
2021-02-01 00:26:11 +0900

    RespondentRepository追加

---
+++ model/respondents.go
@@ -0,0 +1,16 @@
+package model
+
+import "gopkg.in/guregu/null.v3"
+
+// RespondentRepository RespondentのRepository
+type RespondentRepository interface {
+	InsertRespondent(userID string, questionnaireID int, submitedAt null.Time) (int, error)
+	UpdateSubmittedAt(responseID int) error
+	DeleteRespondent(userID string, responseID int) error
+	GetRespondentInfos(userID string, questionnaireIDs ...int) ([]RespondentInfo, error)
+	GetRespondentDetail(responseID int) (RespondentDetail, error)
+	GetRespondentDetails(questionnaireID int, sort string) ([]RespondentDetail, error)
+	GetRespondentsUserIDs(questionnaireIDs []int) ([]Respondents, error)
+	CheckRespondent(userID string, questionnaireID int) (bool, error)
+	CheckRespondentByResponseID(userID string, responseID int) (bool, error)
+}
