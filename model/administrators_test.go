98f51191c7ab0ca406aae27e503fe811ff6700f2
mazrean <shunsuke.w04@gmail.com>
2021-02-23 02:08:53 +0900

    DeleteAdministrators

--- model/administrators_test.go
+++ model/administrators_test.go
@@ -27,6 +27,7 @@ func TestAministrators(t *testing.T) {
 	setupAdministratorTest(t)
 
 	t.Run("InsertAdministrators", insertAdministratorsTest)
+	t.Run("DeleteAdministrators", deleteAdministratorsTest)
 }
 
 func setupAdministratorTest(t *testing.T) {
@@ -183,5 +184,111 @@ func insertAdministratorsTest(t *testing.T) {
 				t.Errorf("no administrator(%s): %w", administrator, err)
 			}
 		}
+	}
+}
+
+func deleteAdministratorsTest(t *testing.T) {
+	t.Helper()
+	t.Parallel()
+
+	assertion := assert.New(t)
+
+	type args struct {
+		questionnaire  Questionnaires
+		administrators []string
+	}
+	type expect struct {
+		isErr bool
+		err   error
+	}
+	type test struct {
+		description string
+		args
+		expect
+	}
+
+	testCases := []test{
+		{
+			description: "questionnaireID: valid, administrator_num: 2",
+			args: args{
+				questionnaire: Questionnaires{
+					Title:       "第1回集会らん☆ぷろ募集アンケート",
+					Description: "第1回集会らん☆ぷろ参加者募集",
+				},
+				administrators: []string{administratorsTestUserIDs[0], administratorsTestUserIDs[1]},
+			},
+		},
+		{
+			description: "questionnaireID: valid, administrator_num: 1",
+			args: args{
+				questionnaire: Questionnaires{
+					Title:       "第1回集会らん☆ぷろ募集アンケート",
+					Description: "第1回集会らん☆ぷろ参加者募集",
+				},
+				administrators: []string{administratorsTestUserIDs[0]},
+			},
+		},
+		{
+			description: "questionnaireID: valid, administrator_num: 0",
+			args: args{
+				questionnaire: Questionnaires{
+					Title:       "第1回集会らん☆ぷろ募集アンケート",
+					Description: "第1回集会らん☆ぷろ参加者募集",
+				},
+				administrators: []string{},
+			},
+		},
+		{
+			description: "questionnaireID: invalid, administrator_num: 1",
+			args: args{
+				questionnaire: Questionnaires{
+					Title:       "第1回集会らん☆ぷろ募集アンケート",
+					Description: "第1回集会らん☆ぷろ参加者募集",
+				},
+				administrators: []string{administratorsTestUserIDs[0]},
+			},
+			expect: expect{
+				isErr: true,
+			},
+		},
+		{
+			description: "questionnaireID: invalid, administrator_num: 0",
+			args: args{
+				questionnaire: Questionnaires{
+					Title:       "第1回集会らん☆ぷろ募集アンケート",
+					Description: "第1回集会らん☆ぷろ参加者募集",
+				},
+				administrators: []string{},
+			},
+			expect: expect{
+				isErr: true,
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		err := db.Create(&testCase.args.questionnaire).Error
+		if err != nil {
+			t.Errorf("failed to create questionnaire(%+v): %w", testCase.args.questionnaire, err)
+		}
+
+		err = administratorImpl.DeleteAdministrators(testCase.args.questionnaire.ID)
+
+		if !testCase.expect.isErr {
+			assertion.NoError(err, testCase.description, "no error")
+		} else if testCase.expect.err != nil {
+			assertion.Equal(testCase.expect.err, err, testCase.description, "error")
+		}
+		if err != nil {
+			continue
+		}
+
+		var administrators []Administrators
+		err = db.Where("questionnaire_id = ?", testCase.args.questionnaire.ID).Find(&administrators).Error
+		if err != nil {
+			t.Errorf("failed to get administrators(%s): %w", testCase.description, err)
+		}
+
+		assertion.Len(administrators, 0, testCase.description, "administrator length")
 	}
 }
