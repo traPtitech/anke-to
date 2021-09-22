26ea8aeab0aca08844fa62d86750c98774cde501
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-09-22 14:27:31 +0900

    unansweredをgomock.any()にしてみた

--- router/users_test.go
+++ router/users_test.go
@@ -681,15 +681,15 @@ func TestGetTargettedQuestionnairesBytraQID(t *testing.T) {
 	// GetTargettedQuestionnaires
 	// success
 	mockQuestionnaire.EXPECT().
-		GetTargettedQuestionnaires(string(userOne), "unanswered", gomock.Any()).
+		GetTargettedQuestionnaires(string(userOne), gomock.Any(), gomock.Any()).
 		Return(targettedQuestionnaires, nil).AnyTimes()
 	// empty
 	mockQuestionnaire.EXPECT().
-		GetTargettedQuestionnaires("empty", "unanswered", gomock.Any()).
+		GetTargettedQuestionnaires("empty", gomock.Any(), gomock.Any()).
 		Return([]model.TargettedQuestionnaire{}, nil).AnyTimes()
 	// failure
 	mockQuestionnaire.EXPECT().
-		GetTargettedQuestionnaires("StatusInternalServerError", "unanswered", gomock.Any()).
+		GetTargettedQuestionnaires("StatusInternalServerError", gomock.Any(), gomock.Any()).
 		Return(nil, errMock).AnyTimes()
 
 	type request struct {
