5ab8b9a8f836c9614498108af4d6ad8396aff875
mazrean <shunsuke.w04@gmail.com>
2021-09-23 17:43:09 +0900

    GET /questionnairesにレートリミット

--- router.go
+++ router.go
@@ -37,7 +37,7 @@ func SetRouting(port string) {
 	{
 		apiQuestionnnaires := echoAPI.Group("/questionnaires")
 		{
-			apiQuestionnnaires.GET("", api.GetQuestionnaires)
+			apiQuestionnnaires.GET("", api.GetQuestionnaires, api.TrapReteLimitMiddlewareFunc())
 			apiQuestionnnaires.POST("", api.PostQuestionnaire)
 			apiQuestionnnaires.GET("/:questionnaireID", api.GetQuestionnaire)
 			apiQuestionnnaires.PATCH("/:questionnaireID", api.EditQuestionnaire, api.QuestionnaireAdministratorAuthenticate)
