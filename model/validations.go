1151a9670978431d55cf74661f678367284065aa
mazrean <shunsuke.w04@gmail.com>
2021-09-29 17:21:22 +0900

    validationsのcontext対応

--- model/validations.go
+++ model/validations.go
@@ -2,12 +2,14 @@
 
 package model
 
+import "context"
+
 // IValidation ValidationのRepository
 type IValidation interface {
-	InsertValidation(lastID int, validation Validations) error
-	UpdateValidation(questionID int, validation Validations) error
-	DeleteValidation(questionID int) error
-	GetValidations(qustionIDs []int) ([]Validations, error)
+	InsertValidation(ctx context.Context, lastID int, validation Validations) error
+	UpdateValidation(ctx context.Context, questionID int, validation Validations) error
+	DeleteValidation(ctx context.Context, questionID int) error
+	GetValidations(ctx context.Context, qustionIDs []int) ([]Validations, error)
 	CheckNumberValidation(validation Validations, Body string) error
 	CheckTextValidation(validation Validations, Response string) error
 	CheckNumberValid(MinBound, MaxBound string) error
