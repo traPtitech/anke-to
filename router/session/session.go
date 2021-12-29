a80a2b27fad30c546915a97073f6ac751eb44ffc
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-29 21:16:58 +0900

    Storeの実装

--- router/session/session.go
+++ router/session/session.go
@@ -4,5 +4,5 @@ import "github.com/labstack/echo/v4"
 
 type IStore interface {
 	GetMiddleware() echo.MiddlewareFunc
-	GetSession(c echo.Context) (Store,error)
+	GetSession(c echo.Context) (*Session,error)
 }
--- router/session/session_impl.go
+++ router/session/session_impl.go
@@ -2,6 +2,9 @@ package session
 
 import (
 	"fmt"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
 	"github.com/srinathgs/mysqlstore"
 	"github.com/traPtitech/anke-to/model"
 )
@@ -10,10 +13,32 @@ type Store struct {
 	store *mysqlstore.MySQLStore
 }
 
+func (s *Store) GetMiddleware() echo.MiddlewareFunc {
+	return session.Middleware(s.store)
+}
+
+func (s *Store) GetSession(c echo.Context) (*Session, error) {
+	sess,err := session.Get("sessions",c)
+	if err != nil {
+		return nil,fmt.Errorf("failed to get session:%w",err)
+	}
+
+	return &Session{
+		c:     c,
+		sess: sess,
+	},nil
+}
+
 func NewStore(sess model.Session) (*Store,error) {
 	store,err := sess.Get()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get session: %w", err)
 	}
 	return &Store{store: store},nil
+}
+
+type Session struct {
+	c echo.Context
+	store mysqlstore.MySQLStore
+	sess *sessions.Session
 }
