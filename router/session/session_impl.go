8228be707fb9be38fa4bb2474df12c6027e45b42
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-30 21:53:32 +0900

    setuserIDとGetUserIDの実装

--- router/session/session_impl.go
+++ router/session/session_impl.go
@@ -18,27 +18,41 @@ func (s *Store) GetMiddleware() echo.MiddlewareFunc {
 }
 
 func (s *Store) GetSession(c echo.Context) (*Session, error) {
-	sess,err := session.Get("sessions",c)
+	sess, err := session.Get("sessions", c)
 	if err != nil {
-		return nil,fmt.Errorf("failed to get session:%w",err)
+		return nil, fmt.Errorf("failed to get session:%w", err)
 	}
 
 	return &Session{
-		c:     c,
+		c:    c,
 		sess: sess,
-	},nil
+	}, nil
 }
 
-func NewStore(sess model.Session) (*Store,error) {
-	store,err := sess.Get()
+func NewStore(sess model.Session) (*Store, error) {
+	store, err := sess.Get()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get session: %w", err)
 	}
-	return &Store{store: store},nil
+	return &Store{store: store}, nil
 }
 
 type Session struct {
-	c echo.Context
+	c     echo.Context
 	store mysqlstore.MySQLStore
-	sess *sessions.Session
+	sess  *sessions.Session
 }
+
+func (s *Session) SetUserID(userID string) {
+	s.sess.Values["userID"] = userID
+}
+
+func (s *Session) GetUserID() (string, error) {
+	userID, ok := s.sess.Values["userID"].(string)
+	if !ok || userID == "" {
+		return "", nil
+	}
+
+	return userID, nil
+}
+
