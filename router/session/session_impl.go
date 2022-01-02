748c4388be9d90379239dc0a45f7090feb324c1c
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-01-02 22:12:42 +0900

    tokenのgettersetter

--- router/session/session_impl.go
+++ router/session/session_impl.go
@@ -2,11 +2,14 @@ package session
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/gorilla/sessions"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 	"github.com/srinathgs/mysqlstore"
 	"github.com/traPtitech/anke-to/model"
+	"golang.org/x/oauth2"
 )
 
 type Store struct {
@@ -38,8 +41,8 @@ func NewStore(sess model.Session) (*Store, error) {
 }
 
 type Session struct {
-	c     echo.Context
-	sess  *sessions.Session
+	c    echo.Context
+	sess *sessions.Session
 }
 
 func (s *Session) SetUserID(userID string) {
@@ -59,11 +62,47 @@ func (s *Session) SetVerifier(verifier string) {
 	s.sess.Values["verifier"] = verifier
 }
 
-func (s *Session) GetVerifier() string  {
-	verifier,ok := s.sess.Values["verifier"].(string)
+func (s *Session) GetVerifier() string {
+	verifier, ok := s.sess.Values["verifier"].(string)
 	if !ok || verifier == "" {
 		return ""
 	}
 
 	return verifier
+}
+
+func (s *Session) SetToken(token *oauth2.Token) {
+	s.sess.Values["access_token"] = token.AccessToken
+	s.sess.Values["token_type"] = token.TokenType
+	s.sess.Values["refresh_token"] = token.RefreshToken
+	s.sess.Values["expiry"] = token.Expiry
+}
+
+func (s *Session) GetToken() (*oauth2.Token, error) {
+	accessToken, ok := s.sess.Values["access_token"].(string)
+	if !ok || accessToken == "" {
+		return nil, ErrNoValue
+	}
+
+	tokenType, ok := s.sess.Values["token_type"].(string)
+	if !ok || tokenType == "" {
+		return nil, ErrNoValue
+	}
+
+	refreshToken, ok := s.sess.Values["refresh_token"].(string)
+	if !ok || refreshToken == "" {
+		return nil, ErrNoValue
+	}
+
+	expiry, ok := s.sess.Values["expiry"].(time.Time)
+	if !ok || expiry.IsZero() {
+		return nil, ErrNoValue
+	}
+
+	return &oauth2.Token{
+		AccessToken:  accessToken,
+		TokenType:    tokenType,
+		RefreshToken: refreshToken,
+		Expiry:       expiry,
+	}, nil
 }
