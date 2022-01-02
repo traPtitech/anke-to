404040ccd7686f0d3f771fa6a90a837e17084eba
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-01-02 22:47:49 +0900

    codeを認可サーバーにGetするハンドラの実装

--- router/oauth.go
+++ router/oauth.go
@@ -1,35 +1,78 @@
 package router
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
 	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
 	"github.com/traPtitech/anke-to/router/session"
 	"golang.org/x/oauth2"
-	"os"
 )
 
 var (
-	clientID = os.Getenv("CLIENT_ID")
+	clientID     = os.Getenv("CLIENT_ID")
 	clientSecret = os.Getenv("CLIENT_SECRET")
-	baseURL = "https://q.trap.jp/api/v3"
+	baseURL      = "https://q.trap.jp/api/v3"
 )
 
 type Oauth struct {
-	config *oauth2.Config
+	config    *oauth2.Config
 	sessStore session.Store
 }
 
 func NewOauth(sessStore session.Store) *Oauth {
 	return &Oauth{
-		config:    &oauth2.Config{
+		config: &oauth2.Config{
 			ClientID:     clientID,
 			ClientSecret: clientSecret,
-			Endpoint:     oauth2.Endpoint{
-				AuthURL:   fmt.Sprintf("%s/%s",baseURL,"oauth2/authorize"),
-				TokenURL:  fmt.Sprintf("%s/%s",baseURL,"oauth2/token"),
+			Endpoint: oauth2.Endpoint{
+				AuthURL:  fmt.Sprintf("%s/%s", baseURL, "oauth2/authorize"),
+				TokenURL: fmt.Sprintf("%s/%s", baseURL, "oauth2/token"),
 			},
-			Scopes:       []string{"read"},
+			Scopes: []string{"read"},
 		},
 		sessStore: sessStore,
 	}
 }
 
+func (o *Oauth) GetCode(c echo.Context) error {
+	sess, err := o.sessStore.GetSession(c)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Errorf("failed to get session:%w", err))
+	}
+
+	verifier := RandomString(90)
+	sess.SetVerifier(verifier)
+
+	state := RandomString(32)
+	sess.SetState(state)
+
+	hash := sha256.Sum256([]byte(verifier))
+	challenge := base64.RawURLEncoding.EncodeToString(hash[:])
+
+	challengeOption := oauth2.SetAuthURLParam("code_challenge", challenge)
+	methodOption := oauth2.SetAuthURLParam("code_challenge_method", "s256")
+
+	authURL := o.config.AuthCodeURL(state, challengeOption, methodOption)
+
+	if err = sess.Save(); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Errorf("failed to save session:%w", err))
+	}
+
+	return c.String(http.StatusOK, authURL)
+}
+
+func RandomString(n int) string {
+	var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letter[rand.Intn(len(letter))]
+	}
+	return string(b)
+}
