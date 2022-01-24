c81c7bf507c5af29f1ad94a7e37dc337b4302272
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-01-24 22:38:06 +0900

    clientにOauthの記述

---
+++ client/src/util/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+export const traQBaseURL = 'https://q.trap.jp/api/v3'
+axios.defaults.baseURL =
+    process.env.NODE_ENV === 'development'
+        ? 'http://localhost:8080/api'
+        : 'https://anke-to.trap.jp/api'
+
+export async function redirect2AuthEndpoint() {
+    const data = (await axios.get('/oauth2/generate/code')).data
+
+    const authorizationEndpointUrl = new URL(data)
+
+    window.location.assign(authorizationEndpointUrl.toString())
+}
+
+export async function getRequest2Callback(to) {
+    return axios.get('/oauth2/callback', {
+        params: {
+            code: to.query.code,
+            state: to.query.state
+        }
+    })
+}
