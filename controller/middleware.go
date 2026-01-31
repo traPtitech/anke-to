2af2e6c737e760ea7a0059081f182057218ad29f
Eraxyso <130852025+Eraxyso@users.noreply.github.com>
2026-01-31 15:44:41 +0000

    test: stop the error for test when get userid

--- controller/middleware.go
+++ controller/middleware.go
@@ -59,8 +59,6 @@ func (m *Middleware) SetUserIDMiddleware(next echo.HandlerFunc) echo.HandlerFunc
 
 		c.Set(userIDKey, userID)
 
-		c.Logger().Errorf("Request: %s", c.Request())
-
 		return next(c)
 	}
 }
